f9b29a439aae5671af08b07377bf798442243695
Lohan <lohan.delomenie@epitech.eu>
2021-07-31 18:39:53 +0200

    Update Naddonzzz.js

--- Naddonzzz.js
+++ Naddonzzz.js
@@ -8,25 +8,23 @@ const allianceChat = () => {
     let msgDiv;
     let dom;
 
-    const updateChat = () => {
-        $.post('index.php?ajaxchatalliance',
-            {
-                Rafraichir : $(this).attr('name'),
-                LastMessage : document.getElementById("LastMessage").value
-            });
-    }
-
     const getMessages = () => {
-        jq.ajax({url: "https://www.natureatwar.fr/chatalliance",
-            success: (data) => {
-                dom = jq(data);
-                const target = jq('#allianceMessages');
-                dom.find('#messagess').each((idx, msg) => {
-                    target.append(msg);
-                }).css('padding-left', '2px');
-                setInterval(updateChat, 10000);
-            }
-        });
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+        const parser = new DOMParser();
+
+        fetch("https://www.natureatwar.fr/chatalliance", requestOptions)
+            .then(response => response.text())
+            .then(result => {
+                const par = parser.parseFromString(result, "text/html");
+                const target = jQuery('#allianceMessagess');
+                target.append(par.getElementsByClassName("cadreTresGrand"));
+                document.getElementsByClassName("cadreTresGrand")[0].classList.remove("cadreTresGrand");
+                target.append("<script>var activite_detectee = false;var intervalle = 10000;var temps_inactivite = 0;var inactivite_persistante = true;var lastExec = 0;function testActivite() {if(activite_detectee) {activite_detectee = false;temps_inactivite = 0;inactivite_persistante = false;}else {if(inactivite_persistante) {temps_inactivite += intervalle;if(temps_inactivite >= 600000){document.getElementById(\"actualiser_auto\").checked = false;document.getElementById(\"Rafraichir\").style.display = \"inline-block\";}}else{inactivite_persistante = true;}}setTimeout('testActivite();', intervalle);}function switchButtonRefresh(){if(document.getElementById(\"actualiser_auto\").checked){document.getElementById(\"Rafraichir\").style.display = \"none\";}else{document.getElementById(\"Rafraichir\").style.display = \"inline-block\";}}$(function() {document.getElementById(\"comment\").focus();testActivite();});$(document).ready(function() { $(\"#comment\").keypress(function (e) {if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {creerMessage ();return false;}});});function actualiserChat (type = 1){if(document.getElementById(\"actualiser_auto\").checked | type == 2){$.post('index.php?ajaxchatalliance',{Rafraichir : $(this).attr('name'),LastMessage : document.getElementById(\"LastMessage\").value},function(data, status){var obj = jQuery.parseJSON(data);if(document.getElementById(\"LastMessage\").value < obj.LastMessage){document.getElementById(\"messagess\").insertAdjacentHTML('afterbegin', obj.courant);if(obj.LastMessage != \"\"){document.getElementById(\"LastMessage\").value = obj.LastMessage;}}});}}setInterval(actualiserChat, 2000);$(document).on(\"click\",\".Envois\",creerMessage);function creerMessage (){$.post('index.php?ajaxchatalliance',{envois : $(this).attr('name'),textChat : $( '.commenter').val(),LastMessage : document.getElementById(\"LastMessage\").value},function(data, status){var obj = jQuery.parseJSON(data);$('.commenter').val('');timeExec = Date.now();if(timeExec - lastExec > 500){lastExec = timeExec;actualiserChat(2);}});}$(document).on(\"click\",\".EnvoyerSignalement\",EnvoyerSignalement);function EnvoyerSignalement(){$.post('index.php?ajaxchat',{EnvoyerSignalement : $(this).attr('name'),raison : $( '.raison').val(),question : $( '.question').val(),idAjax : $( '.IdAjax').val()},function(data, status){var obj = jQuery.parseJSON(data);});}</script>");
+            })
+            .catch(error => console.log('error', error));
     }
 
     const init = () => {
@@ -34,29 +32,26 @@ const allianceChat = () => {
         const msgDivCSS = {
             'overflow-x': 'hidden',
             'background-color': 'khaki',
+            width: '350px',
             right: '0px',
             position: 'fixed',
             'border-left': '2px black solid',
-            'overflow-y': 'scroll',
             height: '100%',
-            'background-image': 'none'
+            'background-image': 'none',
+            'font-height': 'none',
+            'overflow-y': 'scroll',
+            'padding-top': '0px'
         };
 
-        jq('.sideRight').before('<div id="allianceMessages"></div>');
+        jq('.sideRight').before('<div id="allianceMessages"></div>').remove();
         msgDiv = jq("#allianceMessages").attr('class', 'sideRight');
         msgDiv.css(msgDivCSS);
-        msgDiv.append('<h4 style="text-align: center;">Chat d\'alliance</h4><input type="text" id="messageInput"><div id="allianceMessages"></div>');
+        msgDiv.append('<div id="postform"></div><div id="allianceMessagess"></div>');
         getMessages();
-        //jq('#messageInput').css({width: '99%', 'margin-left': '2px'}).keypress((e) => {
-        //    if (e.keyCode === 13) {
-        //        envoiChat();
-        //    }
-        //});
-        setInterval(updateChat, 10000)
     }
     init();
 }
 
-if (window.location.pathname !== "/chatalliance" || window.location.search.length > 0) {
-    allianceChat()
+if ((window.location.pathname !== "/chatalliance" && window.location.pathname !== "/chatgeneral") || window.location.search.length > 0) {
+    allianceChat();
 }
