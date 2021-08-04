f06142e25899710766586559762e1057f5231bdd
Lohan <lohan.delomenie@epitech.eu>
2021-08-04 16:42:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ import convoiMembre from "./src/convoiMembre";
 import maxOuvri from "./src/maxOuvri";
 import totalArmee from "./src/totalArmee";
 import tmpInstallations from "./src/tmpInstallations";
+import tmpRessources from "./src/tmpRessources";
 
 chatAlliance();
 convoiMembre();
 maxOuvri();
 totalArmee();
 tmpInstallations();
+tmpRessources();
