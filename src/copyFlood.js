62072856c8e978c538b785f5a86e9824a4aa64c8
Antoine CAMILLE <antoine.camille@epitech.eu>
2021-08-10 11:10:46 +0200

    Copy function on flood

---
+++ src/copyFlood.js
@@ -0,0 +1,24 @@
+import checkInit from "./checkInit";
+
+const copyFunctions = {
+    'Convois ouvrière': tr => `${tr.children[0].innerText}`,
+    'Attaquer Terrain': tr => `${tr.children[3].children[0].innerText}\n${tr.children[0].innerText}`
+};
+
+const copyFlood = () => {
+    $('#sectionArmee tbody').children().each((idx, tr) => {
+        if (idx === 0) {
+            $(tr).append('<td></td>');
+        } else {
+            const btnId = `copy${idx}`;
+            const type = tr.children[1].innerText.split(' -- ')[0];
+            $(tr).append(`<td><button id="${btnId}">Copier</button></td>`);
+            $(`#${btnId}`).click(() => {
+                const content = [new ClipboardItem({"text/plain": new Blob([copyFunctions[type](tr)], {type: "text/plain"})})];
+                navigator.clipboard.write(content);
+            });
+        }
+    });
+}
+
+export default checkInit([], [], copyFlood)
