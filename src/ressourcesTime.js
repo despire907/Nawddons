a9bfce27f6b9dce13c038a93cb6ede0c8e06f4d9
Pierte <pierre.boscher@epitech.eu>
2021-11-03 00:44:57 +0100

    Update ressourcesTime.js

--- src/ressourcesTime.js
+++ src/ressourcesTime.js
@@ -1,31 +1,66 @@
 import $ from 'jquery';
 import checkInit from "./checkInit";
 
-const ressourcesTime = () => {
+const resourcesTime = () => {
+
+    function resource_calculate(apple) {
+        //catch total apple
+        const totalApple = $("<span name=dynamisme3>").text();
+        console.log(`total apple= ${totalApple}`);
+        //TODO test
+
+        const OToCraft = 666;
+
+
+        return (OToCraft);
+    }
+
+    function time_calculate(time) {
+        //catch total time
+        const OToCraft = 666;
+
+        return (OToCraft);
+    }
 
     function byUnit(count) {
         console.log("__START")
         //TODO check count
-        const tempsUnite = '#tempsUnite' + `${count}`;
-        const ressource = '#ressource' + `${count}`;
-        const timer = '#timer' + `${count}`;
-        const pondreUnite = '#timer' + `${count}`;
+        const tempsUnite = `#tempsUnite${count}`
+        const resource =`#ressource${count}`;
+
+        const timer = `#timer${count}`;
+        const apple = `#apple${count}`
+
+        const pondreUnite = `#pondreUnite${count}`;
 
         const Otimer = $(tempsUnite).text();
+        const AppleTimer = $(resource).text();
 
         $(tempsUnite).remove();
-        $(ressource).after(`<input id="${timer}" value="66" class="form-control ecriture" value="${Otimer}" >bouffe</input>`);
+        $(resource).after(`<input id="${timer}" class="form-control ecriture" value="${Otimer}" >bouffe</input>`);
 
-        const OtoCraft = $(timer).val();
+        $(resource).before(`<input id="${apple}" class="form-control ecriture" value="${AppleTimer}" >bouffe</input>`)
+        $(resource).remove()
 
+        //TODO calculate value
+
+        // $(timer).val()
+
+        const TimeProduction = time_calculate(AppleTimer);
+        const AppleProduction = resource_calculate(AppleTimer);
+
+        const OtoCraft = TimeProduction === 0 ? AppleProduction : TimeProduction;
+
+        console.log(`OtoCraft: ${OtoCraft}`);
         $(pondreUnite).attr("value", OtoCraft);
+
 
         //TODO actualise on change
         $(timer).on("change", function (elem) {
             console.log("change detected");
         })
 
-        console.log("__END")
+        console.log("__END");
     }
 
     const init = () => {
@@ -35,4 +70,4 @@ const ressourcesTime = () => {
     init();
 }
 
-export default checkInit([], ['/ressourcesTime'], ressourcesTime());
+export default checkInit(['/reine'], ['/ressourcesTime'], resourcesTime());
