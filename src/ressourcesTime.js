4b49de9026745abe452821d35ebff53ec077626f
Pierte <pierre.boscher@epitech.eu>
2021-11-03 15:44:28 +0100

    [Feature] craft by time

--- src/ressourcesTime.js
+++ src/ressourcesTime.js
@@ -3,26 +3,27 @@ import checkInit from "./checkInit";
 
 const resourcesTime = () => {
 
-    function byUnit(count) {
-        console.log("__START");
+    function timeByUnit(count) {
         const tempsUnite = `#tempsUnite${count}`;
         const resource = `#ressource${count}`;
 
         const timer = `#timer${count}`;
         const apple = `#apple${count}`;
 
-        const pondreUnite = `#pondreUnite${count}`;
         const pondreUniteId = `pondreUnite${count}`;
 
         const Otimer = $(tempsUnite).text();
         const AppleTimer = $(resource).text();
 
-        $(resource).after(`<input id="${timer}" class="form-control ecriture" value="${Otimer}" >temp</input>`);
-        $(resource).before(`<input id="${apple}" class="form-control ecriture" value="${AppleTimer}" >bouffe</input>`);
+        const position = `#sectionUniteCout${count}`;
+
+
+        $(position).after(`<input id="${timer}" class="form-control ecriture" value="${Otimer}" />`);
+        $(position).after(`<input id="${apple}" class="form-control ecriture" value="${AppleTimer}" />`);
 
         const actualize_apple = `<script>document.getElementById("${apple}").addEventListener("input", 
             function() {
-                const price = ${AppleTimer};
+                const price = parseInt("${AppleTimer}", 10);
                 const input = document.getElementById("${apple}").value;
                 //TODO check spaces in input
                 
@@ -37,21 +38,56 @@ const resourcesTime = () => {
                     OToCraft = OToCraft.substr(0, OToCraft.lastIndexOf('.'))               
 
                 document.getElementById("${pondreUniteId}").value = OToCraft;
+            })
+        </script>`;
+
+        const actualize_time = `<script>document.getElementById("${timer}").addEventListener("input", 
+            function() {
+                var OToCraft = 0;
+                const unitTime = "${Otimer}";
+                const askTime = document.getElementById("${timer}").value.toString(); 
+                var Ominute = 0;
+                var Ohour = 0;
                 
+                if (unitTime.lastIndexOf('m') === -1) {
+                    Ominute = 60 / parseFloat(unitTime);
+                    Ominute = Ominute.toString();
+                    if(Ominute.lastIndexOf('.') !== -1)
+                        Ominute = parseInt(Ominute.substr(0, Ominute.lastIndexOf('.')), 10);
+                    Ohour = 60 * Ominute
+                } else
+                    Ohour = 60 / parseFloat(unitTime);
+                
+                const Oday = 24 * Ohour;               
+                const Omonth = 30 * Oday;
+                const Oyear = 365 * Omonth;
+                
+                if (askTime.lastIndexOf('m') !== -1)
+                    OToCraft = Ominute * askTime.substr(0, askTime.lastIndexOf('m'));
+                else if (askTime.lastIndexOf('h') !== -1)
+                    OToCraft = Ohour * askTime.substr(0, askTime.lastIndexOf('h'));
+                else if (askTime.lastIndexOf('j') !== -1)
+                    OToCraft = Oday * askTime.substr(0, askTime.lastIndexOf('j'));
+                else if (askTime.lastIndexOf('M') !== -1)
+                    OToCraft = Omonth * askTime.substr(0, askTime.lastIndexOf('M'));
+                else if (askTime.lastIndexOf('A') !== -1)
+                    OToCraft = Oyear * askTime.substr(0, askTime.lastIndexOf('A'));
+
+                document.getElementById("${pondreUniteId}").value = OToCraft;;
             })
         </script>`;
 
         $(resource).after(actualize_apple);
+        $(resource).after(actualize_time);
 
-        $(tempsUnite).remove();
-        $(resource).remove();
-        console.log("__END");
+        //$(tempsUnite).remove();
+        //$(resource).remove();
     }
 
     const init = () => {
         var i = 0;
         while (i <= 23) {
-            byUnit(i);
+            timeByUnit(i);
             i++;
         }
     }
