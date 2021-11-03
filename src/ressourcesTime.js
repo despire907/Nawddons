01d721e5b3a75ca9505f282987d3caa47f2770ba
Pierte <pierre.boscher@epitech.eu>
2021-11-03 12:29:31 +0100

    [Feature] craft by apple

--- src/ressourcesTime.js
+++ src/ressourcesTime.js
@@ -3,68 +3,57 @@ import checkInit from "./checkInit";
 
 const resourcesTime = () => {
 
-    function resource_calculate(apple) {
-        //catch total apple
-        const totalApple = $("<span name=dynamisme3>").text();
-        console.log(`total apple= ${totalApple}`);
-        //TODO test
-
-        const OToCraft = 666;
-
-
-        return (OToCraft);
-    }
-
-    function time_calculate(time) {
-        //catch total time
-        const OToCraft = 666;
-
-        return (OToCraft);
-    }
-
     function byUnit(count) {
-        console.log("__START")
-        //TODO check count
-        const tempsUnite = `#tempsUnite${count}`
-        const resource =`#ressource${count}`;
+        console.log("__START");
+        const tempsUnite = `#tempsUnite${count}`;
+        const resource = `#ressource${count}`;
 
         const timer = `#timer${count}`;
-        const apple = `#apple${count}`
+        const apple = `#apple${count}`;
 
         const pondreUnite = `#pondreUnite${count}`;
+        const pondreUniteId = `pondreUnite${count}`;
 
         const Otimer = $(tempsUnite).text();
         const AppleTimer = $(resource).text();
 
+        $(resource).after(`<input id="${timer}" class="form-control ecriture" value="${Otimer}" >temp</input>`);
+        $(resource).before(`<input id="${apple}" class="form-control ecriture" value="${AppleTimer}" >bouffe</input>`);
+
+        const actualize_apple = `<script>document.getElementById("${apple}").addEventListener("input", 
+            function() {
+                const price = ${AppleTimer};
+                const input = document.getElementById("${apple}").value;
+                //TODO check spaces in input
+                
+                //TODO check maximum capacity
+                //const stockPomme = removeExtraSpace(document.getElementsByName("dynamise3")[0].innerHTML);
+                //const maxToCraft = stockPomme / price;
+
+                var OToCraft = input / price;
+                OToCraft = OToCraft.toString();
+                
+                if (OToCraft.lastIndexOf('.') !== -1)
+                    OToCraft = OToCraft.substr(0, OToCraft.lastIndexOf('.'))               
+
+                document.getElementById("${pondreUniteId}").value = OToCraft;
+                
+            })
+        </script>`;
+
+        $(resource).after(actualize_apple);
+
         $(tempsUnite).remove();
-        $(resource).after(`<input id="${timer}" class="form-control ecriture" value="${Otimer}" >bouffe</input>`);
-
-        $(resource).before(`<input id="${apple}" class="form-control ecriture" value="${AppleTimer}" >bouffe</input>`)
-        $(resource).remove()
-
-        //TODO calculate value
-
-        // $(timer).val()
-
-        const TimeProduction = time_calculate(AppleTimer);
-        const AppleProduction = resource_calculate(AppleTimer);
-
-        const OtoCraft = TimeProduction === 0 ? AppleProduction : TimeProduction;
-
-        console.log(`OtoCraft: ${OtoCraft}`);
-        $(pondreUnite).attr("value", OtoCraft);
-
-
-        //TODO actualise on change
-        $(timer).on("change", function (elem) {
-            console.log("change detected");
-        })
-
+        $(resource).remove();
         console.log("__END");
     }
 
     const init = () => {
-        byUnit(1);
+        var i = 0;
+        while (i <= 23) {
+            byUnit(i);
+            i++;
+        }
     }
 
     init();
