0f52d9262c86c7fbec7a7852f213a8d42d598e68
Pierte <pierre.boscher@epitech.eu>
2022-03-11 16:18:20 +0100

    init refacto

--- index.js
+++ index.js
@@ -15,3 +15,24 @@ tmpBatiments();
 copyFlood();
 ressourcesTime();
 boxAlliance();
+
+function actualiseFromTime(box) {
+    const askTime = $(`#timer${box}`).val();
+    const unitTime = $(`#tempsUnite${box}`).text();
+
+    console.log(askTime);
+    console.log(unitTime);
+}
+
+$("#apple1").on("change keyup paste", function(){
+    console.log("Ouvri : modif nb pommes");
+})
+
+$("#timer1").on("change keyup paste", function(){
+    console.log("Ouvri : modif timer");
+    actualiseFromTime(1);
+})
+
+$("#pondreUnite1").on("change keyup paste", function(){
+    console.log("Ouvri : modif nb to craft");
+})
--- src/ressourcesTime.js
+++ src/ressourcesTime.js
@@ -3,12 +3,12 @@ import checkInit from "./checkInit";
 
 const resourcesTime = () => {
 
-    function timeByUnit(count) {
+    function placeBoxByUnit(count) {
         const tempsUnite = `#tempsUnite${count}`;
         const resource = `#ressource${count}`;
 
-        const timer = `#timer${count}`;
-        const apple = `#apple${count}`;
+        const timer = `timer${count}`;
+        const apple = `apple${count}`;
 
         const pondreUniteId = `pondreUnite${count}`;
 
@@ -16,6 +16,8 @@ const resourcesTime = () => {
         const AppleTimer = $(resource).text();
 
         const position = `#sectionUniteCout${count}`;
+
+        $(`#${pondreUniteId}`).val(1);
 
         $(position).after(`
             <div class="img--container">
@@ -38,86 +40,13 @@ const resourcesTime = () => {
                 }
             </style>
         `);
-
-        $(position).remove()
-
-        const actualize_apple = `<script>document.getElementById("${apple}").addEventListener("input", 
-            function() {
-                const price = parseInt("${AppleTimer}", 10);
-                const input = document.getElementById("${apple}").value;
-                
-                //TODO check spaces in input
-                
-                //TODO check maximum capacity
-                //const stockPomme = removeExtraSpace(document.getElementsByName("dynamise3")[0].innerHTML);
-                //const maxToCraft = stockPomme / price;
-
-                var OToCraft = input / price;
-                OToCraft = OToCraft.toString();
-                
-                if (OToCraft.lastIndexOf('.') !== -1)
-                    OToCraft = OToCraft.substr(0, OToCraft.lastIndexOf('.'))               
-
-                if (input === "000") {
-                    OToCraft = 0;
-                }
-                document.getElementById("${pondreUniteId}").value = OToCraft;
-            })
-        </script>`;
-
-        const actualize_time = `<script>document.getElementById("${timer}").addEventListener("input", 
-            function() {
-                var OToCraft = 0;
-                const unitTime = "${Otimer}";
-                const askTime = document.getElementById("${timer}").value.toString(); 
-                var Ominute = 0;
-                var Ohour = 0;
-                
-                if (unitTime.lastIndexOf('m') === -1) {
-                    console.log("Second unit");
-                    var temp = parseFloat(unitTime) 
-                    console.log(temp);
-                    Ominute = 60 / temp;
-                    Ominute = Ominute.toString();
-                    console.log(temp);
-                    if(Ominute.lastIndexOf('.') !== -1)
-                        Ominute = parseInt(Ominute.substr(0, Ominute.lastIndexOf('.')), 10);
-                    Ohour = 60 * Ominute
-                } else {
-                    console.log("Minute unit");
-                    Ohour = 60 / parseFloat(unitTime);
-                }
-                console.log("Ohour = ");
-                console.log(Ohour);
-                const Oday = 24 * Ohour;               
-                const Omonth = 30 * Oday;
-                const Oyear = 365 * Omonth;
-                
-                if (askTime.lastIndexOf('m') !== -1)
-                    OToCraft = Ominute * askTime.substr(0, askTime.lastIndexOf('m'));
-                else if (askTime.lastIndexOf('h') !== -1)
-                    OToCraft = Ohour * askTime.substr(0, askTime.lastIndexOf('h'));
-                else if (askTime.lastIndexOf('j') !== -1)
-                    OToCraft = Oday * askTime.substr(0, askTime.lastIndexOf('j'));
-                else if (askTime.lastIndexOf('M') !== -1)
-                    OToCraft = Omonth * askTime.substr(0, askTime.lastIndexOf('M'));
-                else if (askTime.lastIndexOf('A') !== -1)
-                    OToCraft = Oyear * askTime.substr(0, askTime.lastIndexOf('A'));
-
-                document.getElementById("${pondreUniteId}").value = OToCraft;
-            })
-        </script>`;
-
-        const placement = `#${pondreUniteId}`;
-        $(placement).after(actualize_apple);
-        $(placement).after(actualize_time);
-        $(placement).val(1);
+        //$(position).remove()
     }
 
     const init = () => {
         var i = 0;
         while (i <= 23) {
-            timeByUnit(i);
+            placeBoxByUnit(i);
             i++;
         }
     }
